7dcddfd34b598ee316d1fbd407eec1a3689855f4
Memlisen <emmelie.sundell@hotmail.com>
2017-05-22 17:22:54 +0200

    few changes

--- public/wp-content/themes/learnwordpress/archive.php
+++ public/wp-content/themes/learnwordpress/archive.php
@@ -1,29 +1,73 @@
 <?php
-/*
-Template Name: Archives
-*/
-get_header(); ?>
+/* Template Name: archive */ 
+get_header();
 
-<div id="container">
-	<div id="content" role="main">
-
-		<?php the_post(); ?>
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+if (have_posts()) :
+	
+	?>
+	
+	<h2><?php
+	
+		if ( is_category() ) {
+			single_cat_title();
+		} elseif ( is_tag() ) {
+			single_tag_title();
+		} elseif ( is_author() ) {
+			the_post();
+			echo 'Author Archives: ' . get_the_author();
+			rewind_posts();
+		} elseif ( is_day() ) {
+			echo 'Daily Archives: ' . get_the_date();
+		} elseif ( is_month() ) {
+			echo 'Monthly Archives: ' . get_the_date('F Y');
+		} elseif ( is_year() ) {
+			echo 'Yearly Archives: ' . get_the_date('Y');
+		} else {
+			echo 'Archives:';
+		}
+	
+	?></h2>
+	
+	<?php
+	while (have_posts()) : the_post(); ?>
+	
+	<article class="post">
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		
-		<?php get_search_form(); ?>
+		<p class="post-info"><?php the_time('F j, Y g:i a'); ?> | by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | Posted in
+			
+			<?php
+			
+			$categories = get_the_category();
+			$separator = ", ";
+			$output = '';
+			
+			if ($categories) {
+				
+				foreach ($categories as $category) {
+				
+					$output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>'  . $separator;
+					
+				}
+				
+				echo trim($output, $separator);
+				
+			}
+			
+			?>
+			
+			</p>
 		
-		<h2>Archives by Month:</h2>
-		<ul>
-			<?php wp_get_archives('type=monthly'); ?>
-		</ul>
-		
-		<h2>Archives by Subject:</h2>
-		<ul>
-			 <?php wp_list_categories(); ?>
-		</ul>
+		<?php the_excerpt(); ?>
+	</article>
+	
+	<?php endwhile;
+	
+	else :
+		echo '<p>No content found</p>';
+	
+	endif;
+	
+get_footer();
 
-	</div><!-- #content -->
-</div><!-- #container -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+?>
--- public/wp-content/themes/learnwordpress/page-blog.php
+++ public/wp-content/themes/learnwordpress/page-blog.php
@@ -1,10 +1,10 @@
-	<?php 
+<?php 
 	/* Template Name: Blog */ 
 	get_header();
 
 	query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
 
-	<header class="blog-header">
+	<div class="blog-header">
 		<div class="blog-title">
 			<p><?php bloginfo('name'); ?></p>
 		</div>
@@ -12,25 +12,62 @@
 			<?php $args = array('theme_location' => 'primary');
 			wp_nav_menu($args); ?>
 		</nav>
-	</header>
-	<div class="blog-container">
-		<div class="blog-posts">
-			<?php if (have_posts() ){
-			while (have_posts() ){
-				the_post(); ?>
-				<div class='blog-post'>
-					<h1><?php the_title(); ?></h1>
-					<p>
-						<?php
-						// the_modified_time('F jS, Y');
-						the_time('Y-m-d H:i');
-						the_content(); ?>
-					</p><br>
-					<p>Författare: <?php the_author(); ?></p>
-				</div><?php
-				}
-			} ?>
-		</div><div class="blog-about"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Britskorthaar-64091287828362D7bA.jpg/250px-Britskorthaar-64091287828362D7bA.jpg"><h2>About me</h2><p>Maecenas vestibulum mollis diam. Pellentesque ut neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas</p></div>
 	</div>
+<div class="blogg">
+		<p>Hello</p>
+	</div>
+<div class="blog-about">
+	<img src="http://www.critterbabies.com/wp-content/gallery/parrots/a-baa-baby-parrots-cute.jpg">
+	<h4>About me</h4>
+	<p>Text</p>
+	<?php wp_get_archives( array( 'type' => 'daily', 'limit' => 16) ); ?>
+	
+</div>
+<?php
+if (have_posts()) :
+	
+	while (have_posts()) : the_post(); ?>
+	
+	<article class="post">
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		
+		<p class="post-info"><?php the_time('F j, Y g:i a'); ?> | by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | Posted in
+			
+			<?php
+			
+			$categories = get_the_category();
+			$separator = ", ";
+			$output = '';
+			
+			if ($categories) {
+				
+				foreach ($categories as $category) {
+				
+					$output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>'  . $separator;
+					
+				}
+				
+				echo trim($output, $separator);
+				
+			}
+			
+			?>
+			
+			</p>
+		
+		<?php the_excerpt(); ?>
+	</article>
+	
+	<?php endwhile;
+	
+	else :
+		echo '<p>No content found</p>';
+	
+	endif; ?>
 
-<?php get_footer(); ?>
+
+
+<?php
+get_footer();
+
+?>
